d56b6178f862ad2186670e4747183da2058a73a2
sheelow <104136658+sheelow@users.noreply.github.com>
2022-04-21 15:11:58 +0400

    codeStyle

--- testProject/testProject/ViewController.swift
+++ testProject/testProject/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Property
+    private lazy var labelStart: UILabel = {
+        let labelStart = UILabel()
+        labelStart.text = "Hello Pidor"
+        labelStart.textColor = .red
+        labelStart.font = UIFont(name: "Arial", size: 20)
+        return labelStart
+    }()
+    
+    private var buttonStart: UIButton = {
+        let buttonStart = UIButton()
+        buttonStart.setTitle("Press Me, Pidor", for: .normal)
+        buttonStart.titleLabel?.textColor = UIColor.purple
+        return buttonStart
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureView()
     }
 
-
+    private func configureView() {
+        view.addSubview(labelStart)
+        view.addSubview(buttonStart)
+    }
+    
+    
 }
 
